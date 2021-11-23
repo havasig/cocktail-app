8ee01e372864adc9ecd4ea45939befb214aba317
Gabor Havasi <havasi.gaabor@gmail.com>
2021-11-23 12:56:36 -0800

    Initial Commit

---
+++ cocktail/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  cocktail
+//
+//  Created by Gabor Havasi on 2021. 11. 23..
+//  Copyright © 2021. Gabor Havasi. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
